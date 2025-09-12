047abb03a2b269a2d3b49c08fa53075cfbdc9c0f
tbo <thomas.bolz@gmail.com>
2025-09-12 18:27:27 +0200

    Update Docker image name and specify platform in build script

--- push_to_registry.sh
+++ push_to_registry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-IMAGE_NAME="marketserver"
+IMAGE_NAME="teabeeoh/market_server"
 TAG="latest"
 REGISTRY="docker.fritz.box:5000"
 
 # Image bauen
-docker build -t ${IMAGE_NAME}:${TAG} .
+docker build --platform linux/amd64 -t ${IMAGE_NAME}:${TAG} .
 
 # Image taggen
 docker tag ${IMAGE_NAME}:${TAG} ${REGISTRY}/${IMAGE_NAME}:${TAG}
