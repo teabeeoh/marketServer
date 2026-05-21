f51a82f0a84d8938781a94e11f9034af118b148c
tbo <thomas.bolz@gmail.com>
2026-05-21 09:51:36 +0100

    Update push_to_registry.sh to use version from pyproject.toml for image tagging

--- push_to_registry.sh
+++ push_to_registry.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+set -euo pipefail
 
 IMAGE_NAME="teabeeoh/market_server"
-TAG="latest"
+VERSION=$(grep '^version' pyproject.toml | cut -d'"' -f2)
 REGISTRY="harbor.stocksbot.de"
 #REGISTRY="harbor.fritz.box"
 
-# Image bauen
-docker buildx build --platform linux/amd64,linux/arm64/v8 -t ${IMAGE_NAME}:${TAG} .
+# Image bauen (mit Version- und latest-Tag)
+docker buildx build --platform linux/amd64,linux/arm64/v8 \
+    -t ${IMAGE_NAME}:${VERSION} \
+    -t ${IMAGE_NAME}:latest \
+    .
 
-# Image taggen
-docker tag ${IMAGE_NAME}:${TAG} ${REGISTRY}/${IMAGE_NAME}:${TAG}
+# Images fuer Registry taggen
+docker tag ${IMAGE_NAME}:${VERSION} ${REGISTRY}/${IMAGE_NAME}:${VERSION}
+docker tag ${IMAGE_NAME}:latest ${REGISTRY}/${IMAGE_NAME}:latest
 
 # Login beim Registry
 #docker login ${REGISTRY}
 
-# Image pushen
-docker push ${REGISTRY}/${IMAGE_NAME}:${TAG}
+# Images pushen
+docker push ${REGISTRY}/${IMAGE_NAME}:${VERSION}
+docker push ${REGISTRY}/${IMAGE_NAME}:latest
